4cd29a07913338174295b6efa8ceda234d0a9e28
poojarawat29 <130988642+poojarawat29@users.noreply.github.com>
2023-05-06 18:38:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var index=0;
+function changecolor(){
+    var colors=["red","blue","orange","yellow","green","purple"];
+    document.getElementsByTagName("body")[0].style.background = colors[index++];
+    if(index > colors.length - 1)
+    index = 0;
+}
